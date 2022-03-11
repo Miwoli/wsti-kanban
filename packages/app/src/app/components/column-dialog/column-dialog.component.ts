c75c6890c1384edbbd20d9c1d6533442dd101a52
Michał Woliński <1531829-Miwoli@users.noreply.gitlab.com>
2022-03-11 21:10:05 +0100

    prepare app structure + add angular material

---
+++ packages/app/src/app/components/column-dialog/column-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-column-dialog',
+  templateUrl: './column-dialog.component.html',
+  styleUrls: ['./column-dialog.component.css']
+})
+export class ColumnDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
